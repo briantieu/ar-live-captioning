241fc6a32c797f5fc29d3f8f072ad730083fe58d
Brian Tieu <btieu@princeton.edu>
2022-11-21 14:41:03 -0500

    translate

--- static/js/script.js
+++ static/js/script.js
@@ -41,7 +41,7 @@ if (!window.hasOwnProperty("webkitSpeechRecognition")) {
 const recognition = new webkitSpeechRecognition(); // window.SpeechRecognition();
 // recognition.continuous = true;
 recognition.interimResults = true;
-recognition.lang = "es-MX";
+recognition.lang = 'yue-Hant-HK';
 
 recognition.addEventListener('onerror', (e) => {
     console.error(e);
